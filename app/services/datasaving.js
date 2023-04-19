5035151171c14e047c592dc0e26748180b84c7bf
Ubuntu <ubuntu@ip-172-31-39-198.ap-south-1.compute.internal>
2023-04-19 19:28:15 +0000

    Tried fixing data saving issue

--- app/services/datasaving.js
+++ app/services/datasaving.js
@@ -7,13 +7,15 @@ const {orderBookService} = require('../services/orderBooksService');
     const dataServices = {
         dataSaving: async (event_id, target_price, end_time) => {
             try {
+		console.log(event_id, target_price, end_time, moment(end_time).unix(), moment(new Date).unix());
                 const latestRecord = await btcService.btcLatestRecord()
                 let bap_yes_price, bap_no_price, bap_yes_quantity, bap_no_quantity;   
                 bap_yes_price=await client.get(`bap_yes_price_${event_id}`)
                 bap_no_price=await client.get(`bap_no_price_${event_id}`)
                 bap_yes_quantity=await client.get(`bap_yes_quantity_${event_id}`)
                 bap_no_quantity=await client.get(`bap_no_quantity_${event_id}`)
-                const val_created =  likushData.create({
+		const currentTime = formatDate();
+                const val_created = await likushData.create({
                     trade_time: latestRecord.event_time,
                     Event_Id: event_id,
                     event_currency: "BTCUSDT",
@@ -24,11 +26,14 @@ const {orderBookService} = require('../services/orderBooksService');
                     price: target_price ,
                     time: latestRecord.event_time,
                     close: latestRecord.c_price,
-                    createdAt: formatDate(),
-                    updatedAt: formatDate()
+                    createdAt: currentTime,
+                    updatedAt: currentTime
             })
-            let loop = setTimeout(()=>{dataServices.dataSaving(event_id, target_price, end_time)},1000);
-            let date2 = new Date().toJSON(); 
+	    let loop;
+	    console.log(moment(end_time).unix(), moment(new Date).unix());
+            loop = setTimeout(()=>{dataServices.dataSaving(event_id, target_price, end_time)},1000);
+            let date2 = new Date().toJSON();
+	    console.log(moment(end_time).unix(), moment(date2).unix());
             if(moment(end_time).unix() <= moment(date2).unix()){
                 clearTimeout(loop);
                 orderBookService.unsubscribeToOrderBook(event_id)
--- app/services/orderBooksService.js
+++ app/services/orderBooksService.js
@@ -11,7 +11,8 @@ const orderBookService = {
                 await client.set(`bap_no_price_${EVENT_ID}`,JSON.stringify((response.SELL)[0].price), 'EX', 25 * 60);
                 await client.set(`bap_no_quantity_${EVENT_ID}`,JSON.stringify((response.SELL)[0].quantity), 'EX', 25 * 60);
                 await client.set(`end_time_${EVENT_ID}`, JSON.stringify((END_TIME)), 'EX', 15 * 60);
-            }catch(err){
+            	console.log(response.BUY[0].price, response.SELL[0].price);
+	    }catch(err){
                 console.log("Error: ", err);
             }
         })
