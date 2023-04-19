4fa17e1cd9348b9cc52f8e279f979fe6af31ff05
Mihir-intern-probo <121915526+Mihir-intern-probo@users.noreply.github.com>
2023-04-20 01:30:02 +0530

    Added new worker for fixing data storing bug

--- app/services/datasaving.js
+++ app/services/datasaving.js
@@ -3,43 +3,33 @@ const {client} = require('../utils/redis');
 const formatDate = require('../utils/date');
 const {likushData} = require('../models/likushData');
 const {orderBookService} = require('../services/orderBooksService');
+const { Worker } = require('worker_threads');
 
     const dataServices = {
         dataSaving: async (event_id, target_price, end_time) => {
             try {
-		console.log(event_id, target_price, end_time, moment(end_time).unix(), moment(new Date).unix());
-                const latestRecord = await btcService.btcLatestRecord()
-                let bap_yes_price, bap_no_price, bap_yes_quantity, bap_no_quantity;   
-                bap_yes_price=await client.get(`bap_yes_price_${event_id}`)
-                bap_no_price=await client.get(`bap_no_price_${event_id}`)
-                bap_yes_quantity=await client.get(`bap_yes_quantity_${event_id}`)
-                bap_no_quantity=await client.get(`bap_no_quantity_${event_id}`)
-		const currentTime = formatDate();
-                const val_created = await likushData.create({
-                    trade_time: latestRecord.event_time,
-                    Event_Id: event_id,
-                    event_currency: "BTCUSDT",
-                    tradesYES: bap_yes_quantity==null?0:parseFloat(bap_yes_quantity),
-                    tradesNO: bap_no_quantity==null?0:parseFloat(bap_no_quantity),
-                    Avg_Yes_Price: bap_yes_price==null?0:parseFloat(bap_yes_price),
-                    Avg_No_Price: bap_no_price==null?0:parseFloat(bap_no_price),
-                    price: target_price ,
-                    time: latestRecord.event_time,
-                    close: latestRecord.c_price,
-                    createdAt: currentTime,
-                    updatedAt: currentTime
-            })
-	    let loop;
-	    console.log(moment(end_time).unix(), moment(new Date).unix());
-            loop = setTimeout(()=>{dataServices.dataSaving(event_id, target_price, end_time)},1000);
-            let date2 = new Date().toJSON();
-	    console.log(moment(end_time).unix(), moment(date2).unix());
-            if(moment(end_time).unix() <= moment(date2).unix()){
-                clearTimeout(loop);
-                orderBookService.unsubscribeToOrderBook(event_id)
-            }
+                return new Promise((resolve,reject)=>{
+                    const worker = new Worker('./app/services/RealAlgoService.js',{
+                        workerData:{
+                            event_id: event_id,
+                            target_price, target_price,
+                            end_time: end_time
+                        }
+                    });
+                    worker.on('message' ,(data)=>{
+                        resolve(data);
+                    })
+                    worker.on('error',(data)=>{
+                        reject(data);
+                    })
+                    worker.on('exit',(code)=>{
+                        if(code!=0){
+                            reject(new Error(`Worker file stopped working with code ${code}`))
+                        }
+                    })
+                })
         } catch(err) {
-
+            console.log(err);
         }
     }
 }
