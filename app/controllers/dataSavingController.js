3576cce0c890f625e4ff5bcece2d1352aa6a77c6
Ubuntu <ubuntu@ip-172-31-44-184.ap-south-1.compute.internal>
2023-06-01 21:27:56 +0000

    Added set timeout to individual worker

--- app/controllers/dataSavingController.js
+++ app/controllers/dataSavingController.js
@@ -7,18 +7,18 @@ const {likushData} = require('../models/likushData')
 const moment = require('moment');
 const {client} = require('../utils/redis')
 const formatDate = require('../utils/date');
+const { exec } = require('child_process');
 
 const dataController={
-    saveData: async(req,res)=>{
-        try{
-            let date2 = new Date().toJSON();
+    timeoutFunction: async(req) => {
+	let date2 = new Date().toJSON();
             const latestRecord = await btcService.btcLatestRecord()
-            let bap_yes_price, bap_no_price, bap_yes_quantity, bap_no_quantity;   
-            bap_yes_price=await client.get(`bap_yes_price_${event_id}`)
-            bap_no_price=await client.get(`bap_no_price_${event_id}`)
-            bap_yes_quantity=await client.get(`bap_yes_quantity_${event_id}`)
-            bap_no_quantity=await client.get(`bap_no_quantity_${event_id}`)
-   	    const currentTime = formatDate();
+            let bap_yes_price, bap_no_price, bap_yes_quantity, bap_no_quantity;
+            bap_yes_price=await client.get(`bap_yes_price_${req.body.EVENT_ID}`)
+            bap_no_price=await client.get(`bap_no_price_${req.body.EVENT_ID}`)
+            bap_yes_quantity=await client.get(`bap_yes_quantity_${req.body.EVENT_ID}`)
+            bap_no_quantity=await client.get(`bap_no_quantity_${req.body.EVENT_ID}`)
+            const currentTime = formatDate();
             likushData.create({
                 trade_time: latestRecord.event_time,
                 Event_Id: req.body.EVENT_ID,
@@ -33,6 +33,28 @@ const dataController={
                 createdAt: currentTime,
                 updatedAt: currentTime
             });
+            const currentDate = new Date().toJSON();
+            console.log(currentDate, req.body);
+            if(moment(req.body.END_TIME).unix() <= moment(currentDate).unix()){
+                console.log('Terminating');
+                const command = 'pm2 restart 0';
+                exec(command, (error, stdout, stderr) => {
+                    if (error) {
+                        console.error(`Error executing command: ${error.message}`);
+                        return;
+                    }
+                    if (stderr) {
+                        console.error(`Command execution returned an error: ${stderr}`);
+                        return;
+                    }
+                    console.log(`Command output:\n${stdout}`);
+                });
+            }
+            setTimeout(()=>{dataController.timeoutFunction(req)}, 1000);
+    },
+    saveData: async(req,res)=>{
+        try{
+	    dataController.timeoutFunction(req);
 	    console.log("Started Saving Event Details");
             res.status(200).json({status: " Working "})
         }catch(err){
